9a331785f3e661dbe46a2be027eeea8783644a01
Qmathiss <shaquanamathis2@gmail.com>
2023-06-02 19:15:01 -0400

    edits to html files

--- scripts.js
+++ scripts.js
@@ -1,2 +1,13 @@
 const location = alert ("Please be aware we can only book clients in Georgia California states only!");
 console.log(location);
+
+var alphabetLinks = document.getElementById('alphabetLinks');
+
+for (var i = 0; i < 26; i++) {
+  var listItem = document.createElement('li');
+  var link = document.createElement('a');
+  link.textContent = String.fromCharCode(65 + i);
+  link.href = '#' + String.fromCharCode(65 + i);
+  listItem.appendChild(link);
+  alphabetLinks.appendChild(listItem);
+}
