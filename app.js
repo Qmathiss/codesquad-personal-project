f84bbecfb8a992b33d87417d9acbd5ed3fce01d1
Qmathiss <shaquanamathis2@gmail.com>
2023-06-10 18:55:35 -0400

    added partials and routes for backend project

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
 const express = require("express");
 const morgan = require("morgan");
 const path = require("path");
-const methodOverride = require("method-override");
-// const indexRoutes = require("./routes/index-routes");
 
 const app = express();
 const PORT = 3000;
-app.use(morgan("dev"));
+app.use(morgan("combined"));
 
 app.use(express.static(path.join(__dirname, "public")));
+app.set('view engine','ejs');
 
-app.set("view engine", "ejs");
 
-app.use(methodOverride("_method"));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
-// 
+ app.get("/",(req,res)=> {
+    res.render("pages/index");
+ });
+ app.get("/about", (req, res) => {res.render("pages/about");
+});
+app.get("/book", (req,res) => {
+    res.render("pages/book");
+});
+app.get("/herbals", (req,res)=> {
+    res.render("pages/herbals");
+});
+
+app.get("/herbals/:id", (req,res)=>{
+    res.render("pages/herbals");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on ${PORT}`);
+ });
